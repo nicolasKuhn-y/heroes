96b88180d3da73145c6b9f3fda9684b1b3d5698d
Nicolas Kuhn <nicolaskuhn02@gmail.com>
2021-08-21 17:54:36 -0300

    logeo hecho

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import { AuthContext } from "../auth/AuthContext";
 
 import { LoginScreen } from "../pages/login/LoginScreen";
 
 import { DashBoardRoutes } from "./DashBoardRoutes";
+import { PrivateRoute } from "./PrivateRoute";
 
 export const AppRouter = () => {
+  const {
+    user: { logged },
+  } = useContext(AuthContext);
+
   return (
     <Router>
       <>
         <Switch>
           <Route exact path="/login" component={LoginScreen} />
 
-          <Route path="/" component={DashBoardRoutes} />
+          <PrivateRoute
+            path="/"
+            isUserLogged={logged}
+            component={DashBoardRoutes}
+          />
         </Switch>
       </>
     </Router>
